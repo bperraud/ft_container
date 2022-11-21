0ab76a4f4529bdf7a163a73c573f3a83a2a3348b
bperraud <74benji@live.fr>
2022-11-21 01:36:39 +0100

    iterator for vect

--- Vector.hpp
+++ Vector.hpp
@@ -18,94 +18,113 @@
 template <typename T>
 class vector : public Vect<T> {
 
-public:
-	class iterator;               // make iterator local
-	iterator	it;
+	// iterator class
+	template <typename U>
+	class normal_iterator {
+
+	private:
+		U* _it;
+
+	public:
+		typedef U								value_type;
+		typedef U&								reference;
+		typedef U*								pointer;
+		typedef std::ptrdiff_t					difference_type;
+		typedef std::random_access_iterator_tag	iterator_category;
+
+	public:
+		normal_iterator() : _it(U()) {}
+		normal_iterator( pointer i ) : _it(i) {}
+		normal_iterator( const normal_iterator &other ) : _it( other._it ) {}
+		normal_iterator( U &i ) : _it( &i ) {}
+
+		normal_iterator &operator=( const normal_iterator &other ) {
+			_it = other._it;
+			return *this;
+		}
+		normal_iterator operator+( difference_type n ) const { return _it + n; }
+		/*
+		iterator operator+(difference_type lhs, const iterator &other) {
+			return lhs + other;
+		}
+		*/
+		normal_iterator operator-( difference_type n ) const { return _it - n; }
+		/*
+		difference_type operator-( const Iterator &other ) const {
+			return _p - other._p;
+		}
+		*/
+
+		normal_iterator &operator++() {
+			_it++;
+			return *this;
+		}
+		normal_iterator  operator++( int ) { return _it++; }
+		normal_iterator &operator--() {
+			_it--;
+			return *this;
+		}
+		normal_iterator operator--( int ) { return _it--; }
+
+		reference operator*() { return *_it; }
+		reference operator*() const { return *_it;}
+		pointer operator->() { return _it; }
+		pointer operator->() const { return _it ;};
+
+
+        bool operator>( const normal_iterator &other ) const { return (_it > other.operator->()); };
+        bool operator<( const normal_iterator &other ) const { return (_it < other.operator->()); };
+		bool operator>=( const normal_iterator &other ) const { return (_it >= other.operator->()); };
+		bool operator<=( const normal_iterator &other ) const { return (_it <= other.operator->()); };
+		bool operator==( const normal_iterator& other ) const { return _it == other._it; }
+		bool operator!=( const normal_iterator& other ) const { return _it != other._it; }
+
+		// convert T to const T for non-const to const assignation
+		operator normal_iterator< const U >() const {
+            return ( normal_iterator< const U >( _it ) );
+        }
+	};
 
 public:
+	typedef T                                        value_type;
 
+	/*
+    typedef Allocator                                allocator_type;
+    typedef typename allocator_type::reference       reference;
+    typedef typename allocator_type::const_reference const_reference;
+    typedef typename allocator_type::pointer         pointer;
+    typedef typename allocator_type::const_pointer   const_pointer;
+	*/
+
+    typedef normal_iterator <T>						iterator;
+    typedef normal_iterator <const T>				const_iterator;
+
+	/*
+    typedef ft::reverse_iterator< iterator >         reverse_iterator;
+    typedef ft::reverse_iterator< const_iterator >   const_reverse_iterator;
+    typedef std::ptrdiff_t                           difference_type;
+    typedef std::size_t                              size_type;
+	*/
+
+public:
 	vector () : Vect<T>() {};
-
 	explicit vector (std::size_t d) : Vect<T>(d) {}
 
+	vector( const vector &other ) {
+        *this = other;
+    }
 
-	//const T& operator[] (std::ptrdiff_t idx)  ;
+	vector &operator=( const vector &other ) {
+        return *this;
+    }
 
-	// possible de definir un iterator comme ça ?
-	//typedef T* 			iterator;
-	//typedef const T*	const_iterator;
+	iterator begin() {return iterator(this->operator[](0)) ;}
+	iterator end() { return iterator(this->operator[](Vect<T>::_size-1)) ;}
 
-	//iterator begin() { return &Vect<T>::_val[0]; }
-	//iterator end() { return &Vect<T>::_val[Vect<T>::_size]; }
-
-	iterator begin() { return this->operator[](0) ;}
-	iterator end() { return this->operator[](Vect<T>::_size-1) ;}
+	const_iterator begin() const {;return const_iterator(this->operator[](0)) ;}
+	const_iterator end() const  { return const_iterator(this->operator[](Vect<T>::_size-1)) ;}
 };
 
-
-template <typename T>
-class vector<T>::iterator {
-protected:
-	T* _it;
-
-public:
-	typedef T								value_type;
-	typedef T&								reference;
-	typedef T*								pointer;
-	typedef std::ptrdiff_t					difference_type;
-	typedef std::random_access_iterator_tag	iterator_category;
-
-public:
-	iterator() : _it(T()) {}
-	iterator(const iterator &other) : _it(other._it) {}
-	iterator(pointer i) : _it( i ) {}
-	iterator(T &i) : _it(&i) {}
-
-	//conversion
-	explicit iterator(const T &i) : _it(&i) {}
-
-
-
-	T base() const { return _it; }
-
-	iterator operator+(difference_type n) const {return _it + n;}
-	/*
-	iterator operator+(difference_type lhs, const iterator &other) {
-		return lhs + other;
-	}
-	*/
-
-	iterator operator-(difference_type n) const { return _it - n; }
-
-	/*
-	difference_type operator-( const Iterator &other ) const {
-		return _p - other._p;
-	}
-	*/
-
-	iterator &operator=( const iterator &other ) {
-		_it = other._it;
-		return *this;
-	}
-	iterator &operator++() {
-		_it++;
-		return *this;
-	}
-	iterator  operator++( int ) { return _it++; }
-	iterator &operator--() {
-		_it--;
-		return *this;
-	}
-	iterator operator--( int ) { return _it--; }
-
-	reference operator*() { return *_it; }
-	pointer operator->() { return _it; }
-
-	bool operator==( const reference other ) const { return _it == other._it; }
-	bool operator!=( const reference other ) const { return _it != other._it; }
-	bool operator==( iterator other ) const { return _it == other._it; }
-	bool operator!=( iterator other ) const { return _it != other._it; }
-};
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,50 +3,6 @@
 #include <deque>
 
 
-
-#if 1 //CREATE A REAL STL EXAMPLE
-	#include <map>
-	#include <stack>
-	#include <vector>
-	namespace ft = std;
-#else
-	#include <map.hpp>
-	#include <stack.hpp>
-	#include <vector.hpp>
-#endif
-
-#include <stdlib.h>
-
-#define MAX_RAM 4294967296
-#define BUFFER_SIZE 4096
-struct Buffer
-{
-	int idx;
-	char buff[BUFFER_SIZE];
-};
-
-
-#define COUNT (MAX_RAM / (int)sizeof(Buffer))
-
-template<typename T>
-class MutantStack : public ft::stack<T>
-{
-public:
-	MutantStack() {}
-	MutantStack(const MutantStack<T>& src) { *this = src; }
-	MutantStack<T>& operator=(const MutantStack<T>& rhs)
-	{
-		this->c = rhs.c;
-		return *this;
-	}
-	~MutantStack() {}
-
-	typedef typename ft::stack<T>::container_type::iterator iterator;
-
-	iterator begin() { return this->c.begin(); }
-	iterator end() { return this->c.end(); }
-};
-
 #include "BST.hpp"
 #include "Vect.hpp"
 #include "Cont.hpp"
@@ -54,20 +10,12 @@ public:
 
 #include "Iterator.hpp"
 
-int main(int argc, char** argv) {
-	if (argc != 2)
-	{
-
-		Cont<int> *container = new Cont<int>(30) ;
-
+int main() {
 		/*
 		ft::vector<int>::iterator it;
 		ft::vector<int>::reverse_iterator itr;
 		ft::reverse_iterator<ft::vector<int>::iterator> iter;
 		*/
-
-		Cont<int>::iterator it;
-
 		reverse_iterator<Cont<int>::iterator> iter;
 
 		//Cont<int>::reverse_iterator itr;		// compile pas car pas dans Cont
@@ -75,22 +23,17 @@ int main(int argc, char** argv) {
 		vector<int> *vect = new vector<int>(5);
 
 		vect->operator[](2) = 3;
-		vector<int>::iterator ite;
+		const vector<int>::const_iterator ite = vect->begin();
 
-		for (ite = vect->begin(); ite != vect->end(); ite++) {
-			std::cout << "elem :" << *ite << std::endl;
-		}
+		(void) ite;
+		//vector<int>::iterator ite = vect->begin();
 
 
+		/*
 		Info<int> info;
-
 		std::cout << vect->at(0) << std::endl;
-
-
 		container->insert(4);
-
 		std::cout << "container[0] : " << container->at(0) << std::endl;
-
 		std::cout << "container[0] : " << container[0] << std::endl;
 
 		BST<int> tree;
@@ -100,7 +43,6 @@ int main(int argc, char** argv) {
 		tree.insert(-3);
 		tree.insert(2);
 		tree.insert(89);
-
 		std::vector<int> vec(90);
 
 		std::cout << vec.max_size() << std::endl;
@@ -114,69 +56,7 @@ int main(int argc, char** argv) {
 		std::cerr << "Usage: ./test seed" << std::endl;
 		std::cerr << "Provide a seed please" << std::endl;
 		std::cerr << "Count value:" << COUNT << std::endl;
-		return 1;
-	}
+		*/
 
-	const int seed = atoi(argv[1]);
-
-	srand(seed);
-
-	ft::vector<std::string> vector_str;
-	ft::vector<int> vector_int;
-	ft::stack<int> stack_int;
-	ft::vector<Buffer> vector_buffer;
-	ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
-	ft::map<int, int> map_int;
-
-	for (int i = 0; i < COUNT; i++)
-	{
-		vector_buffer.push_back(Buffer());
-	}
-
-	for (int i = 0; i < COUNT; i++)
-	{
-		const int idx = rand() % COUNT;
-		vector_buffer[idx].idx = 5;
-	}
-	ft::vector<Buffer>().swap(vector_buffer);
-
-	try
-	{
-		for (int i = 0; i < COUNT; i++)
-		{
-			const int idx = rand() % COUNT;
-			vector_buffer.at(idx);
-			std::cerr << "Error: THIS VECTOR SHOULD BE EMPTY!!" <<std::endl;
-		}
-	}
-	catch(const std::exception& e)
-	{
-		//NORMAL ! :P
-	}
-
-	for (int i = 0; i < COUNT; ++i)
-	{
-		map_int.insert(ft::make_pair(rand(), rand()));
-	}
-
-	int sum = 0;
-	for (int i = 0; i < 10000; i++)
-	{
-		int access = rand();
-		sum += map_int[access];
-	}
-	std::cout << "should be constant with the same seed: " << sum << std::endl;
-
-	{
-		ft::map<int, int> copy = map_int;
-	}
-	MutantStack<char> iterable_stack;
-	for (char letter = 'a'; letter <= 'z'; letter++)
-		iterable_stack.push(letter);
-	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
-	{
-		std::cout << *it;
-	}
-	std::cout << std::endl;
-	return (0);
+	return 1;
 }
